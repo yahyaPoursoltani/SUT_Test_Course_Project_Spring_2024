47f281f095f71215c10a42324ef403e49cb22453
Yahya Poursoltani <yahya.poursoltani@gmail.com>
2024-07-03 12:03:33 +0330

    create Root Test Directory.

---
+++ src/test/com/github/mauricioaniche/ck/SetupTet.java
@@ -0,0 +1,11 @@
+package com.github.mauricioaniche.ck;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SetupTet {
+    @Test
+    public void setUp(){
+        Assertions.assertEquals(1,1);
+    }
+}
